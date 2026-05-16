4ed57b56464d59281c1326712daee2e3030bd893
Tejaswini <tejaswininaidu29@gmail.com>
2026-05-16 18:26:58 +0530

    Add github actions

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -13,3 +13,23 @@ resource "azurerm_subnet" "this" {
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = var.subnet_prefixes
 }
+
+resource "azurerm_network_security_group" "this" {
+  name                = "${var.vnet_name}-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = var.tags
+}
